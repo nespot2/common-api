02f9e3bf486d84a9d4281148cd0a130fa841b667
leejaehyuk <nespot2@gmail.com>
2020-02-16 19:47:09 +0900

    project init!

---
+++ src/main/java/com/nespot2/commonapi/profile/service/ProfileService.java
@@ -0,0 +1,9 @@
+package com.nespot2.commonapi.profile.service;
+
+/**
+ * @author nespot2
+ * @versio 0.0.1
+ * @since 2020/02/16
+ **/
+public interface ProfileService {
+}
---
+++ src/main/java/com/nespot2/commonapi/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.nespot2.commonapi.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author nespot2
+ * @versio 0.0.1
+ * @since 2020/02/16
+ **/
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .csrf()
+                .disable();
+
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+}
